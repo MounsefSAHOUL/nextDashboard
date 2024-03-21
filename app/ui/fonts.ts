50af14af96a25929276c750845d6476465cd1931
Mounsef SAHOUL <mounsefsahoul@gmail.com>
2024-03-21 16:02:28 +0100

    Your commit message here

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Averia_Sans_Libre, Lusitana } from 'next/font/google';
+
+export const averia_Sans_Libre = Averia_Sans_Libre({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
